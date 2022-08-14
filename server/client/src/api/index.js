9917f8ccfaf704b46c802906e0f3f8bd81298001
Karishma Shah <89262878+karishmashahh@users.noreply.github.com>
2022-08-14 22:13:16 +0530

    Update index.js

--- server/client/src/api/index.js
+++ server/client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://newww22.herokuapp.com" });
+const API = axios.create({ baseURL: "https://photoflick.herokuapp.com" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
